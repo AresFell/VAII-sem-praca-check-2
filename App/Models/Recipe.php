d32eda3e8835bf5ac704aef2c4d2fec3fbd7acf0
AresFell <sarapapsova@gmail.com>
2023-11-29 22:07:41 +0100

    DATABAZA

---
+++ App/Models/Recipe.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+class Recipe extends Model
+{
+    protected ?int $id;
+    protected ?string $title;
+    protected ?string $ingredients;
+    protected ?string $instructions;
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getIngredients(): ?string
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(?string $ingredients): void
+    {
+        $this->ingredients = $ingredients;
+    }
+
+    public function getInstructions(): ?string
+    {
+        return $this->instructions;
+    }
+
+    public function setInstructions(?string $instructions): void
+    {
+        $this->instructions = $instructions;
+    }
+}
